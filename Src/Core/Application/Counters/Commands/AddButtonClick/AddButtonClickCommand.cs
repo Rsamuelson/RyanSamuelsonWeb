bd755a1203ecd92c5b10c7d9480b770b26015f10
Ryan Samuelson <2015ryansamuelson@gmail.com>
2021-05-05 22:36:53 -0500

    added AddButtonClickCommand

---
+++ Src/Core/Application/Counters/Commands/AddButtonClick/AddButtonClickCommand.cs
@@ -0,0 +1,71 @@
+﻿using Application.Common.Interfaces;
+using Application.Common.TestUtilities;
+using Application.Counters.Exceptions;
+using Common.TestUtilities;
+using Domain.Enums;
+using Domain.Models;
+using FluentAssertions;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Application.Counters.Commands.AddButtonClick
+{
+    public record AddButtonClickCommand : IRequest
+    {
+        internal class AddButtonClickCommandHandler : IRequestHandler<AddButtonClickCommand>
+        {
+            private readonly IRsDbContext _rsDbContext;
+
+            public AddButtonClickCommandHandler(IRsDbContext rsDbContext)
+            {
+                _rsDbContext = rsDbContext;
+            }
+
+            public async Task<Unit> Handle(AddButtonClickCommand request, CancellationToken cancellationToken = default)
+            {
+                var counter = await _rsDbContext.Counters.FindAsync(new object[] { (int)CounterId.ButtonClicks }, cancellationToken);
+
+                if (counter == null) throw new CounterNotFoundExcpetion((int)CounterId.ButtonClicks);
+
+                counter.Count++;
+
+                await _rsDbContext.SaveChangesAsync(cancellationToken);
+
+                return Unit.Value;
+            }
+        }
+    }
+
+    public class GetButtonClickQueryHandlerTests
+    {
+        private readonly IRsDbContext _rsDbContext;
+        private AddButtonClickCommand.AddButtonClickCommandHandler _handler;
+
+        public GetButtonClickQueryHandlerTests()
+        {
+            _rsDbContext = new CommandQueryTestFixture().Context;
+            _handler = new AddButtonClickCommand.AddButtonClickCommandHandler(_rsDbContext);
+        }
+
+        [Fact]
+        public void Hander_WhenCounterDoesNotExistInContext_ThrowsCounterNotFoundExcpetion() =>
+            AssertionUtilities.AssertAsyncMethodThrows<CounterNotFoundExcpetion>(_handler.Handle(new AddButtonClickCommand()));
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(2)]
+        public async Task Hander_returnsButtonClickCount(int testValue)
+        {
+            var counter = new Counter() { CounterId = (int)CounterId.ButtonClicks, Count = testValue };
+            _rsDbContext.Counters.Add(counter);
+            await _rsDbContext.SaveChangesAsync();
+
+            await _handler.Handle(new AddButtonClickCommand());
+
+            counter.Count.Should().Be(testValue + 1);
+        }
+    }
+}
